424436cdbf4fc737aab6d629aeb4279a35653b6b
viriatonova <viriatonova@megaterio.com.br>
2021-06-14 23:12:48 -0300

    organizando dados, função python

--- covid_model.py
+++ covid_model.py
@@ -1,5 +1,21 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 
-data = pd.read_csv('ciberlab_covid/data_base/caso_full.csv')
+data = pd.read_csv('/home/viriato/Documentos/projects/ciberlab_covid/data_base/caso_full.csv', index_col='city')
+#data_palmeiras.to_excel('Palmeiras_casos.xlsx', sheet_name='data_palmeiras', index=False)
 
+
+def data_cidade(data, cidade):
+    ''' 
+    Isolando os dados por municipio
+    '''
+    data_cidade = data.loc[cidade, ['date', 'estimated_population_2019','last_available_confirmed', 
+    'last_available_deaths', 'new_confirmed', 'new_deaths', 'is_repeated']]
+    return data_cidade
+
+#def 
+
+
+if __name__ == '__main__':
+    
+    print(data_cidade(data, "Palmeiras"))
