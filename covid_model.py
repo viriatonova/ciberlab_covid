64238b05dc92afcf494dc2142f21b89dceca5cab
viriatonova <viriatonova@megaterio.com.br>
2021-06-16 22:11:18 -0300

    function data_comparado working

--- covid_model.py
+++ covid_model.py
@@ -1,35 +1,40 @@
 import pandas as pd
 import numpy as np
 
-# download o arquivo caso_full.csv no link -> https://brasil.io/dataset/covid19/files/
-data = pd.read_csv('/home/viriato/megaterio/cybertech/ciberlab_covid/data_base/caso_full.csv', index_col='city')
 
-
-def data_cidade(data, cidade):
+def data_cidade(data, cidade, tags):
     ''' 
     Isolando os dados por municipio.
     '''
-    data_cidade = data.loc[cidade, ['date', 'estimated_population_2019','last_available_confirmed', 
-    'last_available_deaths', 'new_confirmed', 'new_deaths', 'is_repeated']]
+    data_cidade = data.loc[cidade, tags]
     return data_cidade
 
 
-def data_comparado(data, lista_municipios, tag):
+def data_comparado(data, municipios, tags):
     '''
     Comparando dados vários municipios.
-    O parametro tag seleciona a coluna de dados há ser comparada.
     '''
-    for cidade in len(lista_municipios):
-        data_municipio = data.loc[cidade,[tag]]
-        data_municipio.to_excel('municipios_comparados.xlsx', sheet_name=f'data_{cidade}', index=False)
-    return None
+    for cidade in municipios:
+        if cidade == municipios[0]:
+            data_municipios = data.loc[cidade, tags]
+        else:
+            cidade_check = data.loc[cidade, tags]
+            data_municipios = data_municipios.append(cidade_check)
+    return data_municipios
 
 
 if __name__ == '__main__':
     
-    print(data)
+    # download o arquivo caso_full.csv no link -> https://brasil.io/dataset/covid19/files/
+    data = pd.read_csv('/home/viriato/Documentos/projects/ciberlab_covid/data_base/caso_full.csv', index_col='city')
+    
+    tags = ['date', 'estimated_population', 'last_available_confirmed', 'last_available_deaths', 
+    'new_deaths', 'last_available_date']
 
-    #municipios = ['Palmeiras', 'Seabra', 'Lençois', 'Mucugê', 'Andarai']
+    municipios = ['Palmeiras', 'Seabra', 'Lençóis', 'Mucugê', 'Andaraí', 'Ibicoara']
+    
+    chapada_1 = data_comparado(data, municipios, tags)
+    chapada_1.to_excel('Covid_chapada.xlsx', index=True)
+
     '''palmeiras_excel = data_cidade(data, "Palmeiras")
     palmeiras_excel.to_excel('Palmeiras_casos.xlsx', sheet_name='data_palmeiras', index=False)'''
-
