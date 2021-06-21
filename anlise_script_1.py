acd3d6ae1a56637cf0330f4eb5fb20ec92668d1f
viriatonova <viriatonova@megaterio.com.br>
2021-06-21 01:07:43 -0300

    report inicial ok

--- anlise_script_1.py
+++ anlise_script_1.py
@@ -6,16 +6,14 @@ from covid_model import *
 # download o arquivo caso_full.csv no link -> https://brasil.io/dataset/covid19/files/
 data = pd.read_csv('/home/viriato/Documentos/projects/ciberlab_covid/data_base/caso_full.csv')
 
+cidades = ['Palmeiras', 'Seabra', 'Lençóis', 'Mucugê', 'Andaraí', 'Ibicoara']
 tags = ['date', 'last_available_confirmed']
-remove_duplicate = 'last_available_confirmed'
-cidade = 'Palmeiras'
+tag_analise = 'last_available_confirmed'
 
-palmeiras = data_cidade(data, cidade, tags)
-analise_palmeiras = cidade_analise(palmeiras, remove_duplicate)
-analise_palmeiras.to_excel('Covid_palmeiras.xlsx', index=True)
-
-'''
-tags = ['date', 'estimated_population', 'last_available_confirmed', 'last_available_deaths', 'new_deaths', 'last_available_date']
-municipios = ['Palmeiras', 'Seabra', 'Lençóis', 'Mucugê', 'Andaraí', 'Ibicoara']
-chapada_1 = data_select(data, municipios, tags)
-'''
+for cidade in cidades:
+    if cidade == cidades[0]:
+        municipios = data_cidade(data, cidade, tags, tag_analise)
+    else:
+        df = data_cidade(data, cidade, tags, tag_analise)
+        municipios = municipios.append(df)
+municipios.to_excel('chapada_casos.xlsx')
--- covid_model.py
+++ covid_model.py
@@ -2,21 +2,19 @@ import pandas as pd
 import numpy as np
 
 
-def data_cidade(data, cidade, tags):
+def data_cidade(data, cidade, tags, tag_analise):
     ''' 
-    Isolando os dados do municipio por tags.
+    Isolando os dados do municipio por tags 
+    e remove duplocadas de dados
     '''
-    data_cidade = data.set_index('city')
-    data_cidade = data_cidade.loc[cidade, tags]
+    df = data.set_index('city')
+    data_cidade = df.loc[cidade, tags]
+    data_cidade = data_cidade.drop_duplicates(subset=tag_analise, keep='first')
     return data_cidade
 
-def cidade_analise(data_cidade, tags):
-    '''
-    Remove os dados repetidos do data frame 
-    gerado pela função data_cidade
-    '''
-    df = data_cidade.drop_duplicates(subset=tags, keep='first')
-    return df
+
+def data_anual(data_cidade):
+    return
 
 
 def data_select(data, municipios, tags):
@@ -31,3 +29,12 @@ def data_select(data, municipios, tags):
             cidade_check = data.loc[cidade, tags]
             data_municipios = data_municipios.append(cidade_check)
     return data_municipios
+
+if __name__ == '__main__':
+
+    data = pd.read_csv('/home/viriato/Documentos/projects/ciberlab_covid/data_base/caso_full.csv')
+    tags = ['date', 'last_available_confirmed']
+    palmeiras = data_cidade(data, 'Palmeiras', tags, 'last_available_confirmed')
+    print(palmeiras)
+
+    
