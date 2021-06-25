4b38f7aa0f889a8ea00638ea9c8b266cd4cce1cb
ViriatoNova <viriatosampaio@gmail.com>
2021-06-25 15:05:00 -0300

    for loop add

--- anlise_script_1.py
+++ anlise_script_1.py
@@ -1,35 +1,31 @@
 import pandas as pd
 import numpy as np
 from covid_model import *
+from pandas.core.frame import DataFrame
+import openpyxl
 
 
 # download o arquivo caso_full.csv no link -> https://brasil.io/dataset/covid19/files/
-data = pd.read_csv('/home/viriato/Documentos/projects/ciberlab_covid/data_base/caso_full.csv')
+data = pd.read_csv('/home/viriato/megaterio/cybertech/ciberlab_covid/data_base/caso_full.csv')
 
 cidades = ['Palmeiras', 'Seabra', 'Lençóis', 'Mucugê', 'Andaraí', 'Ibicoara']
 tags = ['date', 'last_available_confirmed']
 tag_analise = 'last_available_confirmed'
-df1 = pd.DataFrame((cidades), columns =['Cidades']) 
+
 
 for cidade in cidades:
     if cidade == cidades[0]:
         municipio = data_cidade(data, cidade, tags, tag_analise)
         municipio = municipio.set_index('date')
-        soma = municipio.resample('M').sum()
-        df1['date'] = soma.index
-        df1['last_available_confirmed'] = soma['last_available_confirmed']
+        df = municipio.resample('M').max()
+        df.reset_index(level=0, inplace=True)   
+        df.columns = pd.MultiIndex.from_product([[cidade], df.columns])
     else:
-        df = data_cidade(data, cidade, tags, tag_analise)
-        df = df.set_index('date')
-        soma = df.resample('M').sum()
-        df1['date'] = soma.index
-        df1['last_available_confirmed'] = soma.append[soma['last_available_confirmed']]
-        
-print(df1)
+        df1 = data_cidade(data, cidade, tags, tag_analise)
+        df1 = df1.set_index('date')
+        df2 = df1.resample('M').max()
+        df2.reset_index(level=0, inplace=True) 
+        df2.columns = pd.MultiIndex.from_product([[cidade], df2.columns])
+        df = pd.concat([df, df2])
 
-#municipios = municipios.groupby(['city']).max()
-#municipios['mes'] = pd.DatetimeIndex(municipios['date']).month
-#municipios = municipios.groupby(['city']).max()
-#municipios = municipios.set_index('date')
-#print(municipios)
-#municipios.to_excel('test.xlsx')
+df.to_excel("municipios.xlsx", index=False)  
